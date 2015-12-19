72de21dfcda419be8983d79c08c2783a2b33847e
R.A. Lucas <richard@richardalucas.com>
2015-12-18 23:54:55 -0700

    initial commit

---
+++ apache/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nginxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nginxApp
+ */
+angular.module('nginxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ nginx/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nginxApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nginxApp
+ */
+angular.module('nginxApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
